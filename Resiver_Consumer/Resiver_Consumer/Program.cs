3c03ed99109521176533a2232dc157bae267df0f
Maedeh <KA01@PJ-K001>
2023-06-03 12:00:31 +0330

    Add project files.

---
+++ Resiver_Consumer/Resiver_Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Resiver_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var chanel = connection.CreateModel();
+            chanel.QueueDeclare("myQueue1", false, false, false, null);
+            var consumer = new EventingBasicConsumer(chanel);
+            consumer.Received += (model, eventArg) =>
+            {
+                var body = eventArg.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Resived Message =>:" + message);
+                Thread.Sleep(2000);
+            };
+
+            chanel.BasicConsume("myQueue1", true, consumer);
+            chanel.Close();
+            connection.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Resiver_Consumer/Sender_Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+namespace Sender_Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var chanel = connection.CreateModel();
+            chanel.QueueDeclare("myQueue1", false, false, false, null);
+            for (int i = 0; i < 30; i++)
+            {
+                string message = $"this is a test message :{DateTime.Now.Ticks}";
+                var body = Encoding.UTF8.GetBytes(message);
+                chanel.BasicPublish("", "myQueue1", null, body);
+            }
+            chanel.Close();
+            connection.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
