3bcbc9e67b86e4d94cba04a8d450622868f451b3
Maedeh <KA01@PJ-K001>
2023-06-03 13:59:55 +0330

    changed

--- Resiver_Consumer/Resiver_Consumer/Program.cs
+++ Resiver_Consumer/Resiver_Consumer/Program.cs
@@ -14,14 +14,15 @@ namespace Resiver_Consumer
             factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
             var connection = factory.CreateConnection();
             var chanel = connection.CreateModel();
-            chanel.QueueDeclare("myQueue1", false, false, false, null);
+            // because this queue is created in sender , it is not necessary to difine it 
+            // chanel.QueueDeclare("myQueue1", false, false, false, null);
             var consumer = new EventingBasicConsumer(chanel);
             consumer.Received += (model, eventArg) =>
             {
                 var body = eventArg.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine("Resived Message =>:" + message);
-                Thread.Sleep(2000);
+                Thread.Sleep(1);
             };
 
             chanel.BasicConsume("myQueue1", true, consumer);
--- Resiver_Consumer/Sender_Publisher/Program.cs
+++ Resiver_Consumer/Sender_Publisher/Program.cs
@@ -11,12 +11,14 @@ namespace Sender_Publisher
             factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
             var connection = factory.CreateConnection();
             var chanel = connection.CreateModel();
-            chanel.QueueDeclare("myQueue1", false, false, false, null);
-            for (int i = 0; i < 30; i++)
+            chanel.QueueDeclare("myQueue1", true, false, false, null);
+            for (int i = 0; i < 10000; i++)
             {
                 string message = $"this is a test message :{DateTime.Now.Ticks}";
                 var body = Encoding.UTF8.GetBytes(message);
-                chanel.BasicPublish("", "myQueue1", null, body);
+                var properties = chanel.CreateBasicProperties();
+                properties.Persistent = true;
+                chanel.BasicPublish("", "myQueue1", properties, body);
             }
             chanel.Close();
             connection.Close();
